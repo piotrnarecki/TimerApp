5d15f52dbeae44d6dec2e058fbc0ff63d9e49ea7
piotrnarecki <nareckipiotraleksander@gmail.com>
2021-07-12 16:52:13 +0200

    Initial commit

---
+++ app/src/main/java/pl/piotrnarecki/timerapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package pl.piotrnarecki.timerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TimerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timer)
+    }
+}
